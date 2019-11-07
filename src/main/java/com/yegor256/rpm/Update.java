e4cbca06adb091bb3c250576a4bfdc70eeac64f7
Yegor Bugayenko <yegor256@gmail.com>
2019-11-07 17:41:14 +0300

    #2 logging fixed

--- src/main/java/com/yegor256/rpm/Update.java
+++ src/main/java/com/yegor256/rpm/Update.java
@@ -72,7 +72,7 @@ final class Update {
         }
         final String doc = new XMLDocument(output).toString();
         Files.write(this.xml, doc.getBytes(StandardCharsets.UTF_8));
-        Logger.info(this, "Saved: %s", doc);
+        Logger.debug(this, "Saved:\n%s", doc);
     }
 
 }
