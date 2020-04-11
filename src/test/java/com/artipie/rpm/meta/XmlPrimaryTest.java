a3506ddcc7c8b253e8c267c87407eba0cd2f107d
Victor Noël <victor.noel@crazydwarves.org>
2020-04-11 11:20:54 +0200

    #83 - Add test for XmlPrimary

---
+++ src/test/java/com/artipie/rpm/meta/XmlPrimaryTest.java
@@ -0,0 +1,60 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2020 artipie.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.artipie.rpm.meta;
+
+import java.nio.file.Path;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+import org.junit.jupiter.api.io.TempDir;
+
+/**
+ * Tests for {@link XmlPrimary}.
+ *
+ * @since 0.6
+ */
+public final class XmlPrimaryTest {
+    // @todo #83:30min This test only creates a temporary file for XmlPrimary,
+    //  now some assertion should be added to verify that this class
+    //  can write `primary.xml` file correctly. The example of primary can
+    //  be found at test resources. If #86 is fixed then remove the DisabledOnOs
+    //  annotation.
+    @Test
+    @DisabledOnOs(OS.WINDOWS)
+    public void writesFile(@TempDir final Path temp) throws Exception {
+        final Path file = temp.resolve("primary.xml");
+        try (XmlPrimary prim = new XmlPrimary(file)) {
+            prim.startPackages()
+                .startPackage()
+                .files(
+                    new String[] {"file" },
+                    new String[] {"dir" },
+                    new int[] {0 }
+                ).startFormat()
+                .license("license")
+                .close()
+                .close();
+        }
+    }
+}
