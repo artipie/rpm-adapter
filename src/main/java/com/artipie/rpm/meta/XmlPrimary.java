0db2bb2e7f690f1125dfc0f757f3570d7488091e
Carlos Miranda <carlosmiranda@users.noreply.github.com>
2020-04-13 13:57:55 +0800

    #86 Delete temp file then recreate using ATOMIC_MOVE

--- src/main/java/com/artipie/rpm/meta/XmlPrimary.java
+++ src/main/java/com/artipie/rpm/meta/XmlPrimary.java
@@ -160,7 +160,7 @@ public final class XmlPrimary implements Closeable {
             Files.move(
                 trf,
                 this.tmp,
-                StandardCopyOption.REPLACE_EXISTING
+                StandardCopyOption.ATOMIC_MOVE
             );
         } catch (final XMLStreamException | TransformerException err) {
             throw new IOException("Failed to close", err);
