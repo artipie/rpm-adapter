d362fd0e33bd151aa06f79664090e9919f0a3b19
olenagerasimova <olena.gerasimova@gmail.com>
2020-05-08 11:41:20 +0300

    For #164 fix build

--- src/test/java/com/artipie/rpm/meta/XmlPackagesFileTest.java
+++ src/test/java/com/artipie/rpm/meta/XmlPackagesFileTest.java
@@ -28,7 +28,6 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Random;
 import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
