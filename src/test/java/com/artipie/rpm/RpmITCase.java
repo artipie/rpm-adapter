f2a959fd687c0098cafb42f8d6ed9b47df1565b2
olenagerasimova <olena.gerasimova@gmail.com>
2020-07-03 09:00:45 +0300

    #311 - unused import fix

--- src/test/java/com/artipie/rpm/RpmITCase.java
+++ src/test/java/com/artipie/rpm/RpmITCase.java
@@ -38,7 +38,6 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Locale;
 import java.util.concurrent.ExecutionException;
 import org.apache.commons.io.FileUtils;
