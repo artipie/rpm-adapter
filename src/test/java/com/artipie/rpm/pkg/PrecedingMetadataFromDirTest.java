7106696edb2e59a2ba5061cecc5bf3de1d75e24c
olenagerasimova <olena.gerasimova@gmail.com>
2020-07-07 13:42:24 +0300

    #236 - CR

--- src/test/java/com/artipie/rpm/pkg/PrecedingMetadataFromDirTest.java
+++ src/test/java/com/artipie/rpm/pkg/PrecedingMetadataFromDirTest.java
@@ -28,40 +28,47 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Optional;
 import org.cactoos.list.ListOf;
 import org.hamcrest.Matcher;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.core.AllOf;
+import org.hamcrest.core.IsEqual;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 import org.llorllale.cactoos.matchers.MatcherOf;
 
 /**
- * Test for {@link PrecedingMetadata}.
+ * Test for {@link PrecedingMetadata.FromDir}.
  * @since 0.11
  * @checkstyle LeftCurlyCheck (500 lines)
  */
 class PrecedingMetadataFromDirTest {
 
     @Test
-    void findsFileIfExists(@TempDir final Path temp) throws IOException {
+    void returnsTrueIfFileExists(@TempDir final Path temp) throws IOException {
         final XmlPackage pckg = XmlPackage.PRIMARY;
-        Files.copy(
-            Paths.get("src/test/resources-binary/repodata/primary.xml.gz.example"),
-            temp.resolve(String.format("%s.xml.gz", pckg.filename()))
+        this.copyExampleToTemp(temp, pckg);
+        MatcherAssert.assertThat(
+            new PrecedingMetadata.FromDir(pckg, temp).exists(),
+            new IsEqual<>(true)
+        );
+    }
+
+    @Test
+    void unzipIfFileExists(@TempDir final Path temp) throws IOException {
+        final XmlPackage pckg = XmlPackage.PRIMARY;
+        this.copyExampleToTemp(temp, pckg);
+        final Optional<Path> unziped = new PrecedingMetadata.FromDir(pckg, temp).findAndUnzip();
+        MatcherAssert.assertThat(
+            "Metadata found",
+            unziped.isPresent(),
+            new IsEqual<>(true)
         );
         MatcherAssert.assertThat(
-            new PrecedingMetadata.FromDir(pckg, temp),
-            new AllOf<PrecedingMetadata.FromDir>(
-                new ListOf<Matcher<? super PrecedingMetadata.FromDir>>(
-                    new MatcherOf<PrecedingMetadata.FromDir>(
-                        PrecedingMetadata.FromDir::exists
-                    ),
-                    new MatcherOf<PrecedingMetadata.FromDir>(
-                        meta -> { return meta.findAndUnzip().isPresent(); }
-                    )
-                )
-            )
+            "Metadata unzipped to parent directory",
+            unziped.get().getParent(),
+            new IsEqual<>(temp)
         );
     }
 
@@ -80,6 +87,13 @@ class PrecedingMetadataFromDirTest {
                     )
                 )
             )
+        );
+    }
+
+    private void copyExampleToTemp(final Path temp, final XmlPackage pckg) throws IOException {
+        Files.copy(
+            Paths.get("src/test/resources-binary/repodata/primary.xml.gz.example"),
+            temp.resolve(String.format("%s.xml.gz", pckg.filename()))
         );
     }
 
