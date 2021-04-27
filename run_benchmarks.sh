8402f586a96209f7ac4ce19ab33711ee40c8bb28
ALEXEY ZORCHENKOV <zuoqinrb@163.com>
2021-04-27 10:18:34 +0000

    Remove unused code

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -11,21 +11,11 @@ done
 
 TMPDIR=$(mktemp --directory)
 
-#echo "Currently TMP: ${TMPDIR}"
-#echo "Bundle: ${BUNDLE}"
-#cp /mnt/disk2/projects/rpmtests/bundle1000.tar.gz ${TMPDIR}
-#cp /mnt/disk2/projects/rpmtests/bundle100.tar.gz ${TMPDIR}
-
-#git clone https://github.com/artipie/rpm-adapter.git
-#cd rpm-adapter
 mvn package -Pbench
 wget https://artipie.s3.amazonaws.com/rpm-test/${BUNDLE}.tar.gz
 tar -xvzf ${BUNDLE}.tar.gz -C ${TMPDIR}
 mvn dependency:copy-dependencies
 num=$(echo ${BUNDLE} | cut -c7-11)
-echo "num: ${num}"
-echo ${TMPDIR}/bundle/${num}
 env BENCH_DIR=${TMPDIR}/bundle/${num} java -cp "target/benchmarks.jar:target/classes/*:target/dependency/*" org.openjdk.jmh.Main RpmBench > ${TMPDIR}/out.txt
-#cat benchmark.txt > ${TMPDIR}/out.txt
 tail -2 ${TMPDIR}/out.txt
 rm -rf ${TMPDIR}
