08ef2f7b5792d9aaeda55bd9346cd03868f9f56c
olenagerasimova <olena.gerasimova@gmail.com>
2021-04-29 15:53:20 +0300

    Faster xml event reader and skeleton for new sdk API

---
+++ src/main/java/com/artipie/rpm/RpmMetadata.java
@@ -0,0 +1,129 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2020 artipie.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.artipie.rpm;
+
+import com.artipie.rpm.meta.XmlPackage;
+import com.artipie.rpm.pkg.FilePackage;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Arrays;
+import java.util.List;
+import org.apache.commons.lang3.NotImplementedException;
+
+/**
+ * Rpm metadata class works with xml metadata - adds or removes records about xml packages.
+ * @since 1.4
+ */
+public interface RpmMetadata {
+
+    /**
+     * Removes RMP records from metadata.
+     * @since 1.4
+     */
+    final class Remove {
+
+        /**
+         * Metadata list.
+         */
+        private final List<MetadataItem> items;
+
+        /**
+         * Ctor.
+         * @param items Metadata items
+         */
+        public Remove(final MetadataItem... items) {
+            this.items = Arrays.asList(items);
+        }
+
+        /**
+         * Removes records from metadata by RPMs checksums.
+         * @param checksums Rpms checksums  to remove by
+         */
+        public void perform(final List<String> checksums) {
+            throw new NotImplementedException("Will be implemented later");
+        }
+    }
+
+    /**
+     * Appends RMP records into metadata.
+     * @since 1.4
+     */
+    final class Append {
+
+        /**
+         * Metadata list.
+         */
+        private final List<MetadataItem> items;
+
+        /**
+         * Ctor.
+         * @param items Metadata items
+         */
+        public Append(final MetadataItem... items) {
+            this.items = Arrays.asList(items);
+        }
+
+        /**
+         * Appends records about provided RPMs.
+         * @param packages Rpms to append info about
+         */
+        public void perform(final List<FilePackage> packages) {
+            throw new NotImplementedException("Not implemented yet");
+        }
+    }
+
+    /**
+     * Metadata item.
+     * @since 1.4
+     */
+    final class MetadataItem {
+
+        /**
+         * Xml metadata type.
+         */
+        private final XmlPackage type;
+
+        /**
+         * Xml metadata input stream.
+         */
+        private final InputStream input;
+
+        /**
+         * Xml metadata output, where write the result.
+         */
+        private final OutputStream out;
+
+        /**
+         * Ctor.
+         * @param type Xml type
+         * @param input Xml metadata input stream
+         * @param out Xml metadata output, where write the result
+         */
+        MetadataItem(final XmlPackage type, final InputStream input, final OutputStream out) {
+            this.type = type;
+            this.input = input;
+            this.out = out;
+        }
+    }
+}
