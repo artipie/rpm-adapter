4c54d2546c5f84de4753279992127e154e309829
Alena <olena.gerasimova@gmail.com>
2021-05-11 09:04:26 +0300

    Extracted classes to work with io-streams (#390)
    Part of #388 - As RpmMetadata should work with streams, I've extracted logic with io streams from XmlPrimaryMaid and XmlMaid.ByPkgidAttr

--- src/main/java/com/artipie/rpm/meta/XmlMaid.java
+++ src/main/java/com/artipie/rpm/meta/XmlMaid.java
@@ -84,15 +84,8 @@ public interface XmlMaid {
             final long res;
             try (InputStream in = Files.newInputStream(this.file);
                 OutputStream out = Files.newOutputStream(tmp)) {
-                final XMLEventReader reader = new InputFactoryImpl().createXMLEventReader(in);
-                final XMLEventWriter writer = new OutputFactoryImpl().createXMLEventWriter(out);
-                try {
-                    res = ByPkgidAttr.process(ids, reader, writer);
-                } finally {
-                    writer.close();
-                    reader.close();
-                }
-            } catch (final XMLStreamException ex) {
+                res = new Stream(in, out).clean(ids);
+            } catch (final IOException ex) {
                 throw new XmlException(ex);
             }
             Files.move(tmp, this.file, StandardCopyOption.REPLACE_EXISTING);
@@ -100,41 +93,90 @@ public interface XmlMaid {
         }
 
         /**
-         * Process lines.
-         * @param ids Not valid ids list
-         * @param reader Reader
-         * @param writer Writes
-         * @return Valid packages count
-         * @throws XMLStreamException When error occurs
+         * Implementation of {@link XmlMaid} that accepts streams and cleans xml by pkgid attribute
+         * in package tag. Input/output streams are not closed in this implementation, resources
+         * should be closed from the outside.
+         * @since 1.4
          */
-        private static long process(final List<String> ids, final XMLEventReader reader,
-            final XMLEventWriter writer) throws XMLStreamException {
-            boolean valid = true;
-            long cnt = 0;
-            XMLEvent event;
-            while (reader.hasNext()) {
-                event = reader.nextEvent();
-                if (event.isStartElement()
-                    && event.asStartElement().getName().getLocalPart().equals(ByPkgidAttr.TAG)
-                ) {
-                    if (ids.contains(
-                        event.asStartElement().getAttributeByName(new QName("pkgid")).getValue()
-                    )) {
-                        valid = false;
-                    } else {
+        public static final class Stream implements XmlMaid {
+
+            /**
+             * Input.
+             */
+            private final InputStream input;
+
+            /**
+             * Output.
+             */
+            private final OutputStream out;
+
+            /**
+             * Ctor.
+             * @param input Input
+             * @param out Output
+             */
+            public Stream(final InputStream input, final OutputStream out) {
+                this.input = input;
+                this.out = out;
+            }
+
+            @Override
+            public long clean(final List<String> ids) throws IOException {
+                final long res;
+                try {
+                    final XMLEventReader reader =
+                        new InputFactoryImpl().createXMLEventReader(this.input);
+                    final XMLEventWriter writer =
+                        new OutputFactoryImpl().createXMLEventWriter(this.out);
+                    try {
+                        res = Stream.process(ids, reader, writer);
+                    } finally {
+                        writer.close();
+                        reader.close();
+                    }
+                } catch (final XMLStreamException err) {
+                    throw new IOException(err);
+                }
+                return res;
+            }
+
+            /**
+             * Process lines.
+             * @param ids Not valid ids list
+             * @param reader Reader
+             * @param writer Writes
+             * @return Valid packages count
+             * @throws XMLStreamException When error occurs
+             */
+            private static long process(final List<String> ids, final XMLEventReader reader,
+                final XMLEventWriter writer) throws XMLStreamException {
+                boolean valid = true;
+                long cnt = 0;
+                XMLEvent event;
+                while (reader.hasNext()) {
+                    event = reader.nextEvent();
+                    if (event.isStartElement()
+                        && event.asStartElement().getName().getLocalPart().equals(ByPkgidAttr.TAG)
+                    ) {
+                        if (ids.contains(
+                            event.asStartElement().getAttributeByName(new QName("pkgid")).getValue()
+                        )) {
+                            valid = false;
+                        } else {
+                            valid = true;
+                            cnt = cnt + 1;
+                        }
+                    }
+                    if (valid) {
+                        writer.add(event);
+                    }
+                    if (event.isEndElement()
+                        && event.asEndElement().getName().getLocalPart().equals(ByPkgidAttr.TAG)) {
                         valid = true;
-                        cnt = cnt + 1;
                     }
                 }
-                if (valid) {
-                    writer.add(event);
-                }
-                if (event.isEndElement()
-                    && event.asEndElement().getName().getLocalPart().equals(ByPkgidAttr.TAG)) {
-                    valid = true;
-                }
+                return cnt;
             }
-            return cnt;
         }
 
     }
