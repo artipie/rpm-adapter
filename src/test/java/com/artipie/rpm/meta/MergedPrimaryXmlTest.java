a968304a1f199b293edae73237434de117e59768
olenagerasimova <olena.gerasimova@gmail.com>
2021-05-18 17:44:48 +0300

    Added more tests for MergedPrimaryXml

--- src/test/java/com/artipie/rpm/meta/MergedPrimaryXmlTest.java
+++ src/test/java/com/artipie/rpm/meta/MergedPrimaryXmlTest.java
@@ -35,6 +35,7 @@ import java.nio.file.Path;
 import org.cactoos.map.MapEntry;
 import org.cactoos.map.MapOf;
 import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
 import org.hamcrest.collection.IsEmptyCollection;
 import org.hamcrest.core.IsEqual;
 import org.junit.jupiter.api.Test;
@@ -45,6 +46,7 @@ import org.junit.jupiter.api.Test;
  * @checkstyle ClassDataAbstractionCouplingCheck (500 lines)
  * @checkstyle MagicNumberCheck (500 lines)
  */
+@SuppressWarnings("PMD.AvoidDuplicateLiterals")
 class MergedPrimaryXmlTest {
 
     @Test
@@ -79,6 +81,90 @@ class MergedPrimaryXmlTest {
                     "/*[local-name()='metadata']/*[local-name()='package']/*[local-name()='name' and text()='aom']",
                     "/*[local-name()='metadata']/*[local-name()='package']/*[local-name()='name' and text()='nginx']",
                     "/*[local-name()='metadata']/*[local-name()='package']/*[local-name()='name' and text()='libdeflt1_0']"
+                )
+            );
+        }
+    }
+
+    @Test
+    void addsReplacesRecords() throws IOException {
+        final ByteArrayOutputStream out = new ByteArrayOutputStream();
+        final TestRpm.Libdeflt libdeflt = new TestRpm.Libdeflt();
+        final TestRpm.Time time = new TestRpm.Time();
+        try (InputStream input =
+            new TestResource("repodata/XmlPrimaryCompTest/libdeflt-primary.xml.example")
+                .asInputStream()
+        ) {
+            final MergedPrimaryXml.Result res =
+                new MergedPrimaryXml(input, out, Digest.SHA256).merge(
+                    new MapOf<Path, String>(
+                        new MapEntry<>(time.path(), time.path().getFileName().toString()),
+                        new MapEntry<>(libdeflt.path(), libdeflt.path().getFileName().toString())
+                    )
+                );
+            MatcherAssert.assertThat(
+                "Packages count is incorrect",
+                res.count(),
+                new IsEqual<>(2L)
+            );
+            MatcherAssert.assertThat(
+                "Duplicated packages checksum should contain one checksum",
+                res.checksums(),
+                Matchers.contains("abc123")
+            );
+            final String actual = out.toString(StandardCharsets.UTF_8.name());
+            MatcherAssert.assertThat(
+                "Primary does not have expected packages",
+                actual,
+                XhtmlMatchers.hasXPaths(
+                    // @checkstyle LineLengthCheck (3 lines)
+                    "/*[local-name()='metadata']/*[local-name()='package']/*[local-name()='name' and text()='time']",
+                    "/*[local-name()='metadata']/*[local-name()='package']/*[local-name()='name' and text()='libdeflt1_0']",
+                    "/*[local-name()='metadata']/*[local-name()='package']/*[local-name()='checksum' and text()='47bbb8b2401e8853812e6340f4197252b92463c132f64a257e18c0c8c83ae462']"
+                )
+            );
+        }
+    }
+
+    @Test
+    void appendsSeveralPackages() throws IOException {
+        final ByteArrayOutputStream out = new ByteArrayOutputStream();
+        final TestRpm libdeflt = new TestRpm.Libdeflt();
+        final TestRpm time = new TestRpm.Time();
+        final TestRpm abc = new TestRpm.Abc();
+        try (InputStream input =
+            new TestResource("repodata/XmlPrimaryCompTest/libdeflt-nginx-promary.xml.example")
+                .asInputStream()
+        ) {
+            final MergedPrimaryXml.Result res =
+                new MergedPrimaryXml(input, out, Digest.SHA256).merge(
+                    new MapOf<Path, String>(
+                        new MapEntry<>(time.path(), time.path().getFileName().toString()),
+                        new MapEntry<>(abc.path(), abc.path().getFileName().toString()),
+                        new MapEntry<>(libdeflt.path(), libdeflt.path().getFileName().toString())
+                    )
+                );
+            MatcherAssert.assertThat(
+                "Packages count is incorrect",
+                res.count(),
+                new IsEqual<>(4L)
+            );
+            MatcherAssert.assertThat(
+                "Duplicated packages checksum should contain one checksum",
+                res.checksums(),
+                Matchers.contains("abc123")
+            );
+            final String actual = out.toString(StandardCharsets.UTF_8.name());
+            MatcherAssert.assertThat(
+                "Primary does not have expected packages",
+                actual,
+                XhtmlMatchers.hasXPaths(
+                    // @checkstyle LineLengthCheck (5 lines)
+                    "/*[local-name()='metadata']/*[local-name()='package']/*[local-name()='name' and text()='time']",
+                    "/*[local-name()='metadata']/*[local-name()='package']/*[local-name()='name' and text()='abc']",
+                    "/*[local-name()='metadata']/*[local-name()='package']/*[local-name()='name' and text()='nginx']",
+                    "/*[local-name()='metadata']/*[local-name()='package']/*[local-name()='name' and text()='libdeflt1_0']",
+                    "/*[local-name()='metadata']/*[local-name()='package']/*[local-name()='checksum' and text()='47bbb8b2401e8853812e6340f4197252b92463c132f64a257e18c0c8c83ae462']"
                 )
             );
         }
