56d504e6b4a6e1da098cdf579ea16808047505bd
Alena <olena.gerasimova@gmail.com>
2021-10-13 11:19:20 +0300

    Remove packages from metadata in storage (#443)
    * Added licence to benchmarks pom
    
    * Remove packages from metadata in storage
    
    * Revert "Remove packages from metadata in storage"
    
    This reverts commit ffa65052
    
    * Remove packages from metadata in storage
    
    Co-authored-by: olenagerasimova <olena.gereasimova@gmail.com>

---
+++ src/main/java/com/artipie/rpm/asto/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * The MIT License (MIT) Copyright (c) 2020-2021 artipie.com
+ * https://github.com/artipie/rpm-adapter/LICENSE.txt
+ */
+
+/**
+ * Storage related classes.
+ *
+ * @since 1.9
+ */
+package com.artipie.rpm.asto;
---
+++ src/test/java/com/artipie/rpm/asto/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * The MIT License (MIT) Copyright (c) 2020-2021 artipie.com
+ * https://github.com/artipie/rpm-adapter/LICENSE.txt
+ */
+
+/**
+ * Tests for storage related classes.
+ *
+ * @since 1.9
+ */
+package com.artipie.rpm.asto;
