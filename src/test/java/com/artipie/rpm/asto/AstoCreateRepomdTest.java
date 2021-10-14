82d16e1e66dd81f1eb70bed0dd04b9058a5e7c30
olenagerasimova <olena.gereasimova@gmail.com>
2021-10-14 14:09:10 +0300

    Added test for `AstoCreateRepomd`

--- src/test/java/com/artipie/rpm/asto/AstoCreateRepomdTest.java
+++ src/test/java/com/artipie/rpm/asto/AstoCreateRepomdTest.java
@@ -8,25 +8,33 @@ import com.artipie.asto.Key;
 import com.artipie.asto.Storage;
 import com.artipie.asto.blocking.BlockingStorage;
 import com.artipie.asto.memory.InMemoryStorage;
+import com.artipie.asto.test.TestResource;
 import com.artipie.rpm.Digest;
 import com.artipie.rpm.RepoConfig;
 import com.artipie.rpm.StandardNamingPolicy;
+import com.artipie.rpm.meta.XmlPackage;
 import com.jcabi.matchers.XhtmlMatchers;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import org.cactoos.map.MapEntry;
+import org.cactoos.map.MapOf;
 import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.xmlunit.matchers.CompareMatcher;
 
 /**
  * Test for {@link AstoCreateRepomd}.
  * @since 1.9
+ * @checkstyle ClassDataAbstractionCouplingCheck (500 lines)
  */
+@SuppressWarnings("PMD.AvoidDuplicateLiterals")
 class AstoCreateRepomdTest {
 
     /**
      * Test storage.
      */
-    private Storage storage;
+    private Storage asto;
 
     /**
      * Test config.
@@ -35,22 +43,56 @@ class AstoCreateRepomdTest {
 
     @BeforeEach
     void init() {
-        this.storage = new InMemoryStorage();
-        this.conf = new RepoConfig.Simple(Digest.SHA256, StandardNamingPolicy.PLAIN, true);
+        this.asto = new InMemoryStorage();
+        this.conf = new RepoConfig.Simple(Digest.SHA256, StandardNamingPolicy.SHA256, true);
     }
 
     @Test
     void createsFileWhenStorageIsEmpty() {
-        new AstoCreateRepomd(this.storage, this.conf).perform(Key.ROOT)
+        new AstoCreateRepomd(this.asto, this.conf).perform(Key.ROOT)
             .toCompletableFuture().join();
         MatcherAssert.assertThat(
             new String(
-                new BlockingStorage(this.storage).value(new Key.From("repomd.xml")),
+                new BlockingStorage(this.asto).value(new Key.From("repomd.xml")),
                 StandardCharsets.UTF_8
             ),
             XhtmlMatchers.hasXPaths(
                 "/*[local-name()='repomd']/*[local-name()='revision']"
             )
+        );
+    }
+
+    @Test
+    void createsRepomd() {
+        final Charset charset = StandardCharsets.UTF_8;
+        final BlockingStorage blsto = new BlockingStorage(this.asto);
+        final Key temp = new Key.From("temp");
+        blsto.save(
+            new Key.From(temp, XmlPackage.PRIMARY.name()), "primary.xml.gz".getBytes(charset)
+        );
+        blsto.save(new Key.From(temp, XmlPackage.OTHER.name()), "other.xml.gz".getBytes(charset));
+        blsto.save(
+            new Key.From(temp, XmlPackage.FILELISTS.name()), "filelists.xml.gz".getBytes(charset)
+        );
+        new MapOf<XmlPackage, String>(
+            new MapEntry<>(XmlPackage.PRIMARY, "o_primary_checksum 123"),
+            new MapEntry<>(XmlPackage.OTHER, "o_other_checksum 34"),
+            new MapEntry<>(XmlPackage.FILELISTS, "o_filelists_checksum 76")
+        ).forEach(
+            (key, val) -> blsto.save(
+                new Key.From(temp, key.name(), this.conf.digest().name()),
+                val.getBytes(charset)
+            )
+        );
+        new AstoCreateRepomd(this.asto, this.conf).perform(temp).toCompletableFuture().join();
+        MatcherAssert.assertThat(
+            new TestResource("AstoCreateRepomdTest/repomd-res.xml").asBytes(),
+            CompareMatcher.isSimilarTo(
+                blsto.value(new Key.From(temp, "repomd.xml"))
+            )
+                .ignoreWhitespace()
+                .normalizeWhitespace()
+                .withNodeFilter(node -> "revision".equals(node.getLocalName()))
         );
     }
 
