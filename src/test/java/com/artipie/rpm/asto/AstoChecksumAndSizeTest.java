28c58fdd805536d415f1e4539e6c762e65d96c8e
olenagerasimova <olena.gereasimova@gmail.com>
2021-10-19 12:24:50 +0300

    Calculate checksum and size

---
+++ src/test/java/com/artipie/rpm/asto/AstoChecksumAndSizeTest.java
@@ -0,0 +1,43 @@
+/*
+ * The MIT License (MIT) Copyright (c) 2020-2021 artipie.com
+ * https://github.com/artipie/rpm-adapter/LICENSE.txt
+ */
+package com.artipie.rpm.asto;
+
+import com.artipie.asto.Content;
+import com.artipie.asto.Key;
+import com.artipie.asto.Storage;
+import com.artipie.asto.blocking.BlockingStorage;
+import com.artipie.asto.memory.InMemoryStorage;
+import com.artipie.rpm.Digest;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.core.IsEqual;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test for {@link AstoChecksumAndSize}.
+ * @since 1.9
+ */
+class AstoChecksumAndSizeTest {
+
+    @Test
+    void savesChecksumAndSize() {
+        final Storage asto = new InMemoryStorage();
+        final Charset charset = StandardCharsets.UTF_8;
+        final String item = "storage_item";
+        final byte[] bfirst = item.getBytes(charset);
+        asto.save(new Key.From(item), new Content.From(bfirst)).join();
+        final Digest dgst = Digest.SHA256;
+        new AstoChecksumAndSize(asto, dgst).calculate(new Key.From(item))
+            .toCompletableFuture().join();
+        final BlockingStorage blsto = new BlockingStorage(asto);
+        MatcherAssert.assertThat(
+            new String(blsto.value(new Key.From(item, dgst.name())), charset),
+            new IsEqual<>(String.format("%s %s", DigestUtils.sha256Hex(bfirst), bfirst.length))
+        );
+    }
+
+}
