e5b97f7f377a14842a2364e1cae3767313459cf3
olenagerasimova <olena.gereasimova@gmail.com>
2021-10-26 17:57:17 +0300

    Added `AstoMetadataAdd` to add new packages to metadata

--- src/main/java/com/artipie/rpm/http/RpmUpload.java
+++ src/main/java/com/artipie/rpm/http/RpmUpload.java
@@ -38,6 +38,11 @@ import org.reactivestreams.Publisher;
 final class RpmUpload implements Slice {
 
     /**
+     * Temp key for the packages to add.
+     */
+    public static final Key TO_ADD = new Key.From(".add");
+
+    /**
      * Asto storage.
      */
     private final Storage asto;
