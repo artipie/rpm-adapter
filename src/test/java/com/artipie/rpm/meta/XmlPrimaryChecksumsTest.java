e0f0879fa8eb98467a63f1860da94ebb81978b96
olenagerasimova <olena.gereasimova@gmail.com>
2021-11-02 10:16:02 +0300

    Use xml streams in XmlPrimaryChecksums

--- src/test/java/com/artipie/rpm/meta/XmlPrimaryChecksumsTest.java
+++ src/test/java/com/artipie/rpm/meta/XmlPrimaryChecksumsTest.java
@@ -5,10 +5,9 @@
 package com.artipie.rpm.meta;
 
 import com.artipie.asto.test.TestResource;
-import org.cactoos.list.ListOf;
+import org.cactoos.map.MapEntry;
 import org.hamcrest.MatcherAssert;
-import org.hamcrest.collection.IsIterableContainingInAnyOrder;
-import org.hamcrest.core.IsEqual;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -20,17 +19,14 @@ public class XmlPrimaryChecksumsTest {
     @Test
     void readsChecksums() {
         MatcherAssert.assertThat(
-            new XmlPrimaryChecksums(
-                new TestResource("repodata/primary.xml.example").asPath()
-            ).read(),
-            new IsIterableContainingInAnyOrder<>(
-                new ListOf<org.hamcrest.Matcher<? super String>>(
-                    new IsEqual<>(
-                        "7eaefd1cb4f9740558da7f12f9cb5a6141a47f5d064a98d46c29959869af1a44"
-                    ),
-                    new IsEqual<>(
-                        "54f1d9a1114fa85cd748174c57986004857b800fe9545fbf23af53f4791b31e2"
-                    )
+            new XmlPrimaryChecksums(new TestResource("repodata/primary.xml.example").asPath())
+                .read().entrySet(),
+            Matchers.hasItems(
+                new MapEntry<>(
+                    "aom", "7eaefd1cb4f9740558da7f12f9cb5a6141a47f5d064a98d46c29959869af1a44"
+                ),
+                new MapEntry<>(
+                    "nginx", "54f1d9a1114fa85cd748174c57986004857b800fe9545fbf23af53f4791b31e2"
                 )
             )
         );
