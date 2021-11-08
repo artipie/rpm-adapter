badc98d41fee70c32d99ee7de1cea9761fbe2cbb
olenagerasimova <olena.gereasimova@gmail.com>
2021-11-08 12:11:14 +0300

    unchecked generic array creation

--- src/test/java/com/artipie/rpm/misc/PackagesDiffTest.java
+++ src/test/java/com/artipie/rpm/misc/PackagesDiffTest.java
@@ -21,38 +21,38 @@ class PackagesDiffTest {
     @Test
     void returnsItemsToDelete() {
         final Map<String, String> primary = new MapOf<>(
-            new MapEntry<String, String>("abc.rpm", "abc-checksum"),
-            new MapEntry<String, String>("nginx.rpm", "nginx-checksum")
+            new MapEntry<>("abc.rpm", "abc-checksum"),
+            new MapEntry<>("nginx.rpm", "nginx-checksum")
         );
         final Map<String, String> repo = new MapOf<>(
-            new MapEntry<String, String>("httpd.rpm", "httpd-checksum"),
-            new MapEntry<String, String>("nginx.rpm", "nginx-checksum"),
-            new MapEntry<String, String>("openssh.rpm", "openssh-checksum")
+            new MapEntry<>("httpd.rpm", "httpd-checksum"),
+            new MapEntry<>("nginx.rpm", "nginx-checksum"),
+            new MapEntry<>("openssh.rpm", "openssh-checksum")
         );
         MatcherAssert.assertThat(
             new PackagesDiff(primary, repo).toDelete().entrySet(),
-            Matchers.hasItems(new MapEntry<>("abc.rpm", "abc-checksum"))
+            Matchers.hasItems(new MapEntry<String, String>("abc.rpm", "abc-checksum"))
         );
     }
 
     @Test
     void returnsItemsToAdd() {
         final Map<String, String> primary = new MapOf<>(
-            new MapEntry<String, String>("abc.rpm", "abc-checksum"),
-            new MapEntry<String, String>("nginx.rpm", "nginx-checksum")
+            new MapEntry<>("abc.rpm", "abc-checksum"),
+            new MapEntry<>("nginx.rpm", "nginx-checksum")
         );
         final Map<String, String> repo = new MapOf<>(
-            new MapEntry<String, String>("httpd.rpm", "httpd-checksum"),
-            new MapEntry<String, String>("nginx.rpm", "nginx-checksum"),
-            new MapEntry<String, String>("openssh.rpm", "openssh-checksum"),
-            new MapEntry<String, String>("abc.rpm", "abc-other-checksum")
+            new MapEntry<>("httpd.rpm", "httpd-checksum"),
+            new MapEntry<>("nginx.rpm", "nginx-checksum"),
+            new MapEntry<>("openssh.rpm", "openssh-checksum"),
+            new MapEntry<>("abc.rpm", "abc-other-checksum")
         );
         MatcherAssert.assertThat(
             new PackagesDiff(primary, repo).toAdd().entrySet(),
             Matchers.hasItems(
-                new MapEntry<>("abc.rpm", "abc-checksum"),
-                new MapEntry<>("httpd.rpm", "httpd-checksum"),
-                new MapEntry<>("openssh.rpm", "openssh-checksum")
+                new MapEntry<String, String>("abc.rpm", "abc-checksum"),
+                new MapEntry<String, String>("httpd.rpm", "httpd-checksum"),
+                new MapEntry<String, String>("openssh.rpm", "openssh-checksum")
             )
         );
     }
